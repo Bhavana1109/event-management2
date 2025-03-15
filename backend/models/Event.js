2ea6ab25472c1de9dbca5f5a19911fe1aac927de
Bhavana1109 <yelagandalabhavana@gmail.com>
2025-03-15 16:06:54 +0530

    pushing files

--- backend/models/Event.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const EventSchema = new mongoose.Schema({
-  title: String,
-  date: String,
-  location: String,
-}, { timestamps: true });
-
-module.exports = mongoose.model("Event", EventSchema);
